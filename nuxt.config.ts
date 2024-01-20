7589bc7dbfdefebef178a0cdb863d3547db3ed52
VitAndrGuid <vitor.guidorizzi@hotmail.com>
2024-01-20 11:13:17 -0400

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["@vite-pwa/nuxt", "@nuxtjs/tailwindcss"],
+
   devtools: { enabled: true },
-  typescript: {
-    shim: false,
-  },
+
+  typescript: { shim: false },
+
+  pwa: {},
 });
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="text-red-500">xd</div>
+</template>
